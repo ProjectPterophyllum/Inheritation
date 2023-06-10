696a698e30b3d4bb69544bf3acefeeab3bdbe603
Alexey Bolshakov <bad2600@outlook.com>
2023-06-10 11:37:22 +0300

    UPD

--- AbstractBaseClass/main.cpp
+++ AbstractBaseClass/main.cpp
@@ -8,12 +8,32 @@ class Vehicle
 	int speed;
 	const int MAX_SPEED;
 public:
+	Vehicle(int speed = 0, int max_speed = 250) :speed(speed), MAX_SPEED(max_speed){}
 	virtual void move() = 0; //Pure virtual func;
 };
 class GroundVehicle : public Vehicle{};
+class Car :public GroundVehicle
+{
+public:
+	void move()
+	{
+		cout << "Машина ездит на колесах:" << endl;
+	}
+};
+class Buldozer : public GroundVehicle
+{
+public:
+	void move()
+	{
+		cout << "Бульдозер ездит на гусеницах:" << endl;
+	}
+};
 void main()
 {
 	setlocale(LC_ALL, "");
 	//Vehicle A; //Cannot instantiate Abstract Class;
-	GroundVehicle gv;
+	Car A;
+	A.move();
+	Buldozer amd;
+	amd.move();
 }
